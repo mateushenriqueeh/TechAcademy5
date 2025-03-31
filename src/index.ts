4c39c3ae03c29a3d5938bcbe16697333a8a3e11a
mateushenriqueeh <mateushenrique1314.mg@gmail.com>
2025-03-31 19:37:17 -0300

    Conectando ao banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import sequelize from "./config/database";
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,15 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Banco de dados conectado");
+  })
+  .catch((error) => {
+    console.log("Erro ao conectar o Banco");
+  });
 
 app.listen(port, () => {
   console.log("Servidor rodando na porta", port);
